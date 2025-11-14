8d0d7b308200ba100360ba4ca8f987074fe02d6b
Edric Yahir <aguilarhernandezedricyahir@gmail.com>
2025-11-13 23:58:14 -0600

    Fix: Estructura correcta para Vercel

--- APIRest/index.js
+++ APIRest/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import taskRoutes from "./routes/taskRoutes.js";
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/tasks.js";
 
 dotenv.config();
 
